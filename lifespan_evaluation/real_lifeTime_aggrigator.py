d020125ddee2d5e94db4500405b528c437c6ab7e
soheilzi <soheil.zibakhsh@gmail.com>
2022-03-30 15:18:52 +0430

    moved files of all ub into its folder and results where optained

--- lifespan_evaluation/real_lifeTime_aggrigator.py
+++ lifespan_evaluation/real_lifeTime_aggrigator.py
@@ -5,21 +5,24 @@ from os import walk
 
 channels_path = sys.argv[1]
 number_of_channels = int(sys.argv[2])
+number_of_folders = int(sys.argv[3])
 
-filenames = next(walk(channels_path), (None, None, []))[2]  # [] if no file
-# filenames.sort()
-print(len(filenames))
 count_ub = [0 for i in range(number_of_channels)]
 unbalancing_time_agg = [0 for i in range(number_of_channels)]
 
-for file in filenames:
-    df = pd.read_csv(channels_path + file)
-    # print(file)
-    for id in range(number_of_channels):
-        unbalancing_time = df[df['id'] == id]['unbalancing_time'].iloc[0] / 1000
-        if unbalancing_time != 0:
-            unbalancing_time_agg[id] += unbalancing_time
-            count_ub[id] += 1
+for i in range(number_of_folders):
+    filenames = next(walk(channels_path + "_" + str(i)), (None, None, []))[2]  # [] if no file
+    # filenames.sort()
+    print(len(filenames))
+
+    for file in filenames:
+        df = pd.read_csv(channels_path + "_" + str(i) + "/" + file)
+        # print(file)
+        for id in range(number_of_channels):
+            unbalancing_time = df[df['id'] == id]['unbalancing_time'].iloc[0] / 1000
+            if unbalancing_time != 0:
+                unbalancing_time_agg[id] += unbalancing_time
+                count_ub[id] += 1
     
     
 f = open("lifeTimes_real.csv", "w+")
