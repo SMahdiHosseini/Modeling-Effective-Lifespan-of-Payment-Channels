f98bbd7e64ed19bc200f60d93e3f4ea28fe90068
SMahdiHosseini <smahdihosseini4@gmail.com>
2022-04-12 11:31:37 +0430

    network generator must be changed to predict lifespan from Mrates file!

---
+++ lifespan_evaluation/network_2_120_/mrates_generator.py
@@ -0,0 +1,25 @@
+from ntpath import join
+import sys
+import numpy as np
+
+seed = int(sys.argv[1])
+number_of_nodes = int(sys.argv[2])
+sparse_coef = float(sys.argv[3])
+average_rate = int(sys.argv[4]) # in second
+number_of_pairs = int(((number_of_nodes ** 2) - number_of_nodes) * sparse_coef)
+
+MRates = [[-1 for i in range(number_of_nodes)] for i in range(number_of_nodes)]
+
+for i in range(number_of_pairs):
+    x, y = np.random.choice(range(number_of_nodes), 2, False)
+    MRates[x][y] = int(np.random.normal(average_rate))
+    MRates[y][x] = int(np.random.normal(average_rate))
+
+
+f = open("Mrates.csv", "w+")
+for i in range(number_of_nodes):
+    for j in range(number_of_nodes):
+        if MRates[i][j] == -1:
+            MRates[i][j] = 0
+
+    f.write(",".join([str(MRates[i][j]) for j in range(number_of_nodes)]) + "\n")
